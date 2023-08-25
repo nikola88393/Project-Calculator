d7e74e1880733af5f1e70c1f535074286a2f4aa8
Nikola <nikola88393@gmail.com>
2023-08-25 14:55:40 +0300

    Calculator is now usable. Operations work as intended

--- script.js
+++ script.js
@@ -24,11 +24,70 @@ function add(a, b){
 }
 function subtract(a, b){
     return parseInt(a) - parseInt(b);
-}
+}   
 function multiply(a, b){
     return parseInt(a) * parseInt(b);
 }
 function divide(a, b){
-    return parseInt(a) / parseInt(b);
+    let x = parseInt(a) / parseInt(b);
+    return x.toPrecision(7);
 }
+
+function populateDisplay(){
+    const display = document.querySelector('.displayContainer');
+    const digits = document.querySelectorAll('.digits');
+    const operators = document.querySelectorAll('.operator');
+    const equals = document.querySelector('.equals');
+    const changeSign = document.querySelector('.negativeToPositive');
+    const clear = document.querySelector('.clear');
+    let num1 = null, num2 = null, Operator = null;
+    digits.forEach(digit => {
+        digit.addEventListener('click', function() {
+            if(num1 !== null){
+                display.textContent = null;
+            }
+            display.textContent += digit.textContent;
+        });
+    });
+    operators.forEach(operator =>{
+        operator.addEventListener('click', function(){
+            if(num1 !== null && num2 === null){
+                num2 = display.textContent;
+                display.textContent = operate(num1, num2, Operator);
+                num1 = display.textContent;
+                num2 = null;
+                Operator = operator.textContent;
+            }
+            else{
+                num1 = display.textContent;
+                display.textContent = null;
+                Operator = operator.textContent;
+            }
+            
+        });
+    });
+    equals.addEventListener('click', function(){
+        if(num1 !== null){
+            num2 = display.textContent;
+            display.textContent = operate(num1, num2, Operator);
+            num1 = display.textContent;
+        }
+    });
+    clear.addEventListener('click', function(){
+        display.textContent = null;
+        num1 = null;
+        num2 = null;
+        Operator = null;
+    })
+    changeSign.addEventListener('click', function(){
+        if(display.textContent === null){
+            display.textContent = '-' + display.textContent;
+        }
+    })
+}
+// 1 Create precentage method
+// 2 create sign change method 
+// 3 find a way to make the coma work
+
+populateDisplay();
 
